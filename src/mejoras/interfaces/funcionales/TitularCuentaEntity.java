6090e038b5b0369bbf4de9c61238eea3bea44fe2
daniel florez <daniel0223@hotmail.es>
2024-04-02 18:50:08 -0500

    interfaces funcionales

---
+++ src/mejoras/interfaces/funcionales/TitularCuentaEntity.java
@@ -0,0 +1,15 @@
+package mejoras.interfaces.funcionales;
+
+import java.util.List;
+
+public class TitularCuentaEntity {
+
+    private int id;
+
+    private String nombre;
+
+    private  String apellido;
+
+    private List<BeneficiarioEntity> beneficiarioEntity;
+
+}
