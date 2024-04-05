368940e6ab04c8934779018c18ea0b0711fd2370
daniel florez <daniel0223@hotmail.es>
2024-04-04 20:08:42 -0500

    clases anidadas

---
+++ src/clases/anidadas/anonymous/classes/Saludar.java
@@ -0,0 +1,6 @@
+package clases.anidadas.anonymous.classes;
+
+public interface Saludar {
+    void sayHello();
+    void sayBye();
+}
---
+++ src/genericos/ImpersorLista.java
@@ -0,0 +1,6 @@
+package genericos;
+
+public class ImpersorLista {
+
+    //metodo
+}
