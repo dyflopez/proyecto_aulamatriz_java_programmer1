284e050ad0be89d91b0af19890531adc5baf981b
daniel florez <daniel0223@hotmail.es>
2024-04-25 19:12:45 -0500

    adding mysql jdbc

---
+++ src/proyecto/curso/config/ConnectionDB.java
@@ -0,0 +1,32 @@
+package proyecto.curso.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionDB {
+    private  String url = "jdbc:mysql://aulamatriz-db.cnjpj1xnrslk.us-east-1.rds.amazonaws.com:3306/";
+    private  String password="aulamatriz1234";
+    private  String user="admin";
+
+    private String dbName = "user_db";
+
+    /**
+     * Checked Exceptions : Son las que el compilador/java obliga a manejar
+     * unChecked Exception : Son excepciones que ocurren en tiempo de ejecucion .
+     *     NullPointerException
+     */
+    public ConnectionDB()   {
+        try{
+            Connection connection = DriverManager.getConnection(this.url+this.dbName,this.user,this.password);
+            System.out.println("Se conecto correctamente");
+        }catch (Exception e){
+            System.err.println(e.getCause() + " " + e.getMessage());
+        }finally {
+            System.out.println("Se  ejecuto metodo para la conexion con la base de datos");
+        }
+    }
+
+    public static void main(String[] args) {
+        ConnectionDB connectionDB = new ConnectionDB();
+    }
+}
